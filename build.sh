23b5d6feb54f829ba30a67e26395fe0ce1763552
ilhamgusti <ilham.wibowo51@gmail.com>
2023-03-20 12:15:35 +0700

    bump version to 1.2.0

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t ilhamgusti/wallpaper-ai-api-endpoints:1.1.0 .
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t ilhamgusti/wallpaper-ai-api-endpoints:1.2.0 .
